0355a91917683853e3f7ff37f56e8dc77ce98743
ychoy <yanyinchoy@gmail.com>
2017-02-02 08:40:25 -0800

    updated jquery to  set limit for animation, starting point, and status for win or tie

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,52 @@
 // sanity check
 console.log("App.js is connected!");
 
-$(document).ready(function(){
+$(document).ready(function() {
+	var enterprise = {
+		top: $("#enterprise").animate().top,
+		left: $("#enterprise").animate().left,
+		keyCode: 39
 
- setInterval(moveStarship, 20);
- var keys = {}
+	}
 
- $(document).keydown(function(e) {
-     keys[e.keyCode] = true;
- });
+	var warbird = {
+		top: $("#warbird").animate().top,
+		left: $("#warbird").animate().left,
+		keyCode: 68
+	}
 
- $(document).keyup(function(e) {
-     delete keys[e.keyCode];
- });
+	$(document).keydown(function move(e) {
+			if (e.keyCode == enterprise.keyCode) {
+				$("#enterprise").animate({top: enterprise.top, left: enterprise.left += 25}, 0);
+
+			}
+			if(e.keyCode == warbird.keyCode) {
+				$("#warbird").animate({top: warbird.top, left: warbird.left +=25}, 0);
+			}
+
+		 if (enterprise.left >= 800 && warbird.left >= 800) {
+				$(".status").text("You tied!");
+				setTimeout(startNewGame, 100)  // Return the starshis to start
+			} else if (warbird.left >= 800) {
+				$("status").text("The Romulans won the race to the final frontier.");
+				setTimeout(startNewGame, 100) // Return the starship to start
+			} else if (enterprise.left >= 800) {
+				$(".status").text("The Federation won the race to the final frontier.");
+				setTimeout(startNewGame, 100) // Return the starship to start
+	    }
+  })
+
+	$("button").click(startNewGame);
+
+	function startNewGame() {
+		$("#enterprise").animate({top: 20, left: -10}); // Move the starship to the starting position (left)
+		enterprise.top = 25;  // simulate launch
+		enterprise.left = 0;
+		$("#warbird").animate({top: 10, left: -10}) // Move the starship to the starting position (left)
+		enterprise.top = 10;  // simulate launch
+		warbird.top = 25; // simulate launch
+		warbird.left = 0;
+	}
 
 
-
- function moveStarship() {
-
-     for (var direction in keys) {
-         if (!keys.hasOwnProperty(direction)) continue;
-
-         if (direction == 37) {
-             $("#enterprise").animate({left: "-=5"}, 0);
-         }
-         if (direction == 38) {
-             $("#enterprise").animate({top: "-=5"}, 0);
-         }
-         if (direction == 39) {
-             $("#enterprise").animate({left: "+=5"}, 0);
-         }
-         if (direction == 40) {
-             $("#enterprise").animate({top: "+=5"}, 0);
-         }
-     }
-     for (var direction in keys) {
-         if (!keys.hasOwnProperty(direction)) continue;
-         if (direction == 65) {
-             $("#warbird").animate({left: "-=5"}, 0);
-         }
-         if (direction == 87) {
-             $("#warbird").animate({top: "-=5"}, 0);
-         }
-         if (direction == 68) {
-             $("#warbird").animate({left: "+=5"}, 0);
-         }
-         if (direction == 83) {
-             $("#warbird").animate({top: "+=5"}, 0);
-         }
-     }
- }
 })
